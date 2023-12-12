db16407fb5cb34ac46522e1adc364dcf0b643adc
Abdulrahman Aldayhani <148686321+Ab700700@users.noreply.github.com>
2023-12-12 14:54:52 +0300

    Add files via upload

---
+++ camino/src/main/java/com/example/camino/Controller/CommentController.java
@@ -0,0 +1,50 @@
+package com.example.camino.Controller;
+
+import com.example.camino.Model.Comment;
+import com.example.camino.Service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/camino/comment")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("/get")
+    public ResponseEntity getComments(){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getComments());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addComment(@RequestBody @Valid Comment comment){
+        commentService.addCommment(comment);
+        return ResponseEntity.status(HttpStatus.OK).body("Comment added");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateComment(@RequestBody @Valid Comment comment, @PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(id,comment));
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteComment(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.deleteComment(id));
+    }
+
+    @PostMapping("/commentto/{cid}/{uid}")
+    public ResponseEntity commentto(@PathVariable Integer cid, @PathVariable Integer uid, @RequestBody @Valid Comment comment){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.comment(cid,uid,comment));
+    }
+
+    @GetMapping("/getcommentto/{cid}")
+    public ResponseEntity getCommentto(@PathVariable Integer cid){
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getCommentsto(cid));
+    }
+}
---
+++ camino/src/main/java/com/example/camino/Controller/LikeController.java
@@ -0,0 +1,39 @@
+package com.example.camino.Controller;
+
+import com.example.camino.Model.LikePost;
+import com.example.camino.Service.LikeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/camino/like")
+@RequiredArgsConstructor
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @GetMapping("/get")
+    public ResponseEntity getLikes(){
+        return ResponseEntity.status(HttpStatus.OK).body(likeService.getLikes());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addLike(@RequestBody @Valid LikePost likePost){
+        likeService.add(likePost);
+        return ResponseEntity.status(HttpStatus.OK).body("Like adde");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateLike(@RequestBody @Valid LikePost likePost, @PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(likeService.update(id, likePost));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable Integer id){
+        return ResponseEntity.status(HttpStatus.OK).body(likeService.delete(id));
+    }
+}
